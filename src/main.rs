7826406010ff697559dfe9483a19a1be9763deb9
Marius <marius.ihring@icloud.com>
2023-01-30 23:06:11 +0100

    feat/getAllUsersRoute to understand basics

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,26 @@
 mod prisma;
-
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use prisma::user;
 use prisma::PrismaClient;
 use prisma_client_rust::NewClientError;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello).service(getAllUsers))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
 
+// All The Routes
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello World")
 }
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(format!("{} Marius", req_body))
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there")
-}
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let client: Result<PrismaClient, NewClientError> = PrismaClient::_builder().build().await;
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+#[get("/api/getAllUsers")]
+async fn getAllUsers() -> impl Responder {
+    let client = prisma::new_client().await.unwrap();
+    let users: Vec<user::Data> = client.user().find_many(vec![]).exec().await.unwrap();
+    let json = serde_json::to_string(&users).unwrap();
+    HttpResponse::Ok().body(json)
 }
