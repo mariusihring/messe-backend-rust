8d3ff7d8921c481cb48f93f0a981c32d995f9897
Marius Ihring <marius.ihring@icloud.com>
2023-01-31 21:15:31 +0100

    feat/added routes in seperate file instead of main

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,16 @@
-mod prisma;
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use prisma::user;
 
+pub mod routes;
+use routes::{get_all_users, get_specific_user, submit};
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
             .service(get_all_users)
             .service(get_specific_user)
+            .service(submit)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
-}
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello from the Messe API")
-}
-#[get("/api/getAllUsers")]
-async fn get_all_users() -> impl Responder {
-    let client = prisma::new_client().await.unwrap();
-    let users: Vec<user::Data> = client
-        .user()
-        .find_many(vec![])
-        .with(user::interests::fetch())
-        .with(user::company_data::fetch())
-        .exec()
-        .await
-        .unwrap();
-    let json = serde_json::to_string(&users).unwrap();
-    HttpResponse::Ok().body(json)
-}
-#[get("/api/getSpecificUser/{user_id}")]
-async fn get_specific_user(user_id: web::Path<i32>) -> impl Responder {
-    let user_id: i32 = user_id.to_owned();
-    let client = prisma::new_client().await.unwrap();
-    let user = client
-        .user()
-        .find_first(vec![user::id::equals(user_id)])
-        .with(user::interests::fetch())
-        .with(user::company_data::fetch())
-        .exec()
-        .await
-        .unwrap();
-    let json = serde_json::to_string(&user).unwrap();
-    HttpResponse::Ok().body(json)
 }
---
+++ src/routes.rs
@@ -0,0 +1,36 @@
+mod prisma;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use prisma::user;
+use serde::Deserialize;
+
+#[get("/api/getAllUsers")]
+pub async fn get_all_users() -> impl Responder {
+    let client = prisma::new_client().await.unwrap();
+    let users: Vec<user::Data> = client
+        .user()
+        .find_many(vec![])
+        .with(user::interests::fetch())
+        .with(user::company_data::fetch())
+        .exec()
+        .await
+        .unwrap();
+    let json = serde_json::to_string(&users).unwrap();
+    HttpResponse::Ok().body(json)
+}
+
+#[get("/api/getSpecificUser/{user_id}")]
+pub async fn get_specific_user(user_id: web::Path<i32>) -> impl Responder {
+    let user_id: i32 = user_id.to_owned();
+    let client = prisma::new_client().await.unwrap();
+    let user = client
+        .user()
+        .find_first(vec![user::id::equals(user_id)])
+        .with(user::interests::fetch())
+        .with(user::company_data::fetch())
+        .exec()
+        .await
+        .unwrap();
+    let json = serde_json::to_string(&user).unwrap();
+    HttpResponse::Ok().body(json)
+}
+
