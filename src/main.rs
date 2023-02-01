5c34bcc845d727dca8e1d91d1f26934233fe8135
marc <marc-felix.schmitz@protonmail.com>
2023-02-01 19:20:07 +0100

    uwu

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 
 pub mod routes;
-use routes::{get_all_users, get_specific_user};
-#[actix_web::main]
+use routes::{get_all_users, get_specific_user, generate_data};
+#[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_all_users).service(get_specific_user))
+    HttpServer::new(|| App::new().service(get_all_users)
+                                .service(get_specific_user)
+                                .service(generate_data))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,12 @@
 mod prisma;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::{fs::File, io::BufReader};
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder, Result};
 use prisma::user;
+use serde::Deserialize;
+use std::fs;
+use std::io::{self, prelude::*};
+use std::io::prelude::*;
 
 #[get("/api/getAllUsers")]
 pub async fn get_all_users() -> impl Responder {
@@ -31,5 +37,57 @@ pub async fn get_specific_user(user_id: web::Path<i32>) -> impl Responder {
         .unwrap();
     let json = serde_json::to_string(&user).unwrap();
     HttpResponse::Ok().body(json)
+}
+
+#[derive(Deserialize)]
+struct Info {
+    ammount_user: i32,
+}
+
+#[derive(Debug, Deserialize)]
+struct Person {
+    name: String,
+    family_name: String
+}
+
+#[post("/api/generateData")]
+async fn generate_data(info: web::Json<Info>) -> impl Responder {
+    let client = prisma::new_client().await.unwrap();
+
+    let data = fs::read_to_string("./src/names.json")
+    .expect("Unable to read file");
+
+    let person: Vec<Person> = serde_json::from_str(&data).expect("JSON was not well-formatted");
+    for user in &person {
+        let new_user = client
+        .user()
+        .create(
+            user.family_name.to_owned(),
+            user.name.to_owned(),
+            format!("{:?}@{:?}.com", &user, &user.family_name),
+            "231323123132131".to_owned(),
+            vec![]
+        )
+        .exec()
+        .await;
+
+        match new_user {
+            Ok(_) => HttpResponse::Ok(),
+            Err(_) => HttpResponse::Ok()
+        };
+        
+    }
+    
+
+
+    HttpResponse::Ok().body(format!("Welcome {}!", info.ammount_user))
+}
+
+
+
+/// deserialize `Info` from request's body
+#[post("/submit")]
+async fn submit(info: web::Json<Info>) -> Result<String> {
+    Ok(format!("Welcome {}!", info.ammount_user))
 }
 
